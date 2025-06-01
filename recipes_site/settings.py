fca6c4534ab6a339fba8f75826ff4e4ad658293f
KU3Mi4 <kuzmichov@bk.ru>
2025-06-01 15:17:07 +0300

    edit host settings

--- recipes_site/settings.py
+++ recipes_site/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-e=d05v-9w@^w5!x0j*#1l3z-n#p0y@g1i^pv=a&lzh54#%pz2)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
