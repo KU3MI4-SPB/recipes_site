445c0a1ca0afab1d7a166996a29ccad426e48001
KU3Mi4 <kuzmichov@bk.ru>
2025-06-02 04:25:48 +0300

    static_fail

--- recipes_site/settings.py
+++ recipes_site/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e=d05v-9w@^w5!x0j*#1l3z-n#p0y@g1i^pv=a&lzh54#%pz2)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['185.221.215.10', '127.0.0.1']
 
